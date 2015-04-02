de9eddbc3d676c59891e3250ffdb326ebb88a65b
Thierry <guichon.thierry@orbcomm.com>
2015-04-02 15:12:32 -0400

    Addition of SymbolMappingQpsk and renaming of the Sdpsk classes

--- modulators.h
+++ modulators.h
@@ -1,7 +1,9 @@
 /*-----------------------------------------------------------------------------
 @file
 
-Definition of all DSP routines which perform modualtion
+Definition of all DSP routines which perform symbol mapping and
+modulation.
+
 The naming conventions are as follows:
 Class names: ClassName
 Class member data : dataMember
@@ -60,22 +62,23 @@ namespace dsptl
 {
 
 	/*-----------------------------------------------------------------------------
-	SDPSK modulator
+	Mapping of bits in a sequence of complex symbols for SDPSK
 
 	Takes a bit pattern and generates the modulation symbols for an SDPSK modulation\n
 	The output of the modulator is always complex with the type specified as template
-	parameter
+	parameter.\n
+	There are as many output symbols as the number of input bits
 
-	@tparam T Type of each of the output of the complex sample
+	@tparam T Each output sample is of the type complex<T>
 
 
 	------------------------------------------------------------------------------*/
 	template<class T>
-	class ModulatorSdpsk
+	class SymbolMapperSdpsk
 	{
 	public:
 		/// Default constructor
-		ModulatorSdpsk()
+		SymbolMapperSdpsk()
 		{
 			//static_assert(std::is_signed<T>::value);
 			T amplitude;
@@ -89,7 +92,8 @@ namespace dsptl
 
 		}
 
-		/// Performs the modulation as required
+		/// Performs the modulation as required.
+		/// The input bits can be either 0,1 or -1,1
 		void step(const std::vector<int8_t> & bits , std::vector<std::complex<T> > & out)
 		{
 			const int N = 4;
@@ -99,6 +103,74 @@ namespace dsptl
 				// Generates a state from 0 to 3
 				state += (bits[i] > 0)? 1 : (N-1);
 				state = state % N;
+				// Map the state to the correct amplitude
+				std::complex<T> tmp = map[state];
+				out[i] = tmp;
+				
+			}
+		}
+
+		/// Resets the state to a known value
+		void reset()
+		{
+			state = 0;
+		}
+
+
+	private:
+		int state;
+		std::complex<T> map[4];
+
+
+	};
+
+
+	/*-----------------------------------------------------------------------------
+	Mapping of bits into symbols for QPSK or OQPSK modulations
+
+	Takes a bit pattern and generates the modulation symbols for a non-differential quadrature
+	modulation\n
+	The output of the symbol mapping is complex<T>.\n
+	The number of bits at the input of the step() function must be even and the
+	number of returned symbols is 1/2 the number of input bits
+
+	@tparam T Each output symbol is of the type complex<T>
+
+
+	------------------------------------------------------------------------------*/
+	template<class T>
+	class SymbolMapperQpsk
+	{
+	public:
+		/// Default constructor
+		SymbolMapperQpsk()
+		{
+			//static_assert(std::is_signed<T>::value);
+			T amplitude;
+			amplitude = dsptl_private::ModAmplitude < T > {}.value;
+			// Create the constellation map Gray encoding is used
+			map[0] = { amplitude, amplitude };
+			map[1] = { static_cast<T>(-amplitude), amplitude };
+			map[3] = {  static_cast<T>(-amplitude),  static_cast<T>(-amplitude) };
+			map[2] = { amplitude,  static_cast<T>(-amplitude) };
+
+			reset();
+
+		}
+
+		/// Performs the modulation as required
+		/// The bits vector can either consists of 0, 1 or -1, 1
+		void step(const std::vector<int8_t> & bits , std::vector<std::complex<T> > & out)
+		{
+			const int N = 4;
+			assert(bits.size() == 2 * out.size());
+
+			// Bits are grouped 
+			for (size_t i = 0; i < bits.size() /2 ; i++)
+			{
+				// The sequence of bits 00110110 become the symbols 0, 3, 1, 2
+				state = ((bits[2 * i] > 0 ?1:0) << 1) | (bits[2 * i + 1] > 0? 1:0);
+
 				// Map the state to the correct amplitude
 				std::complex<T> tmp = map[state];
 				out[i] = tmp;
