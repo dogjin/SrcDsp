b9ac69dc0edafce470b7f97d97ae5530dc7e6ce8
Thierry Guichon <guichon.thierry@orbcomm.com>
2017-04-10 22:15:08 +0000

    SRC_DSP: WIP attempts to make limitScale work for complex and numbers
    (cherry picked from commit 18b58ba584e904709b6fc6c25e0744d3659872a9)

--- dsp_complex.h
+++ dsp_complex.h
@@ -19,6 +19,7 @@ Class member function: functionMember
 #include <complex>
 #include <vector>
 #include <limits>
+#include <type_traits>
 
 
 std::complex<int32_t> operator*(std::complex<int32_t> a, std::complex<int16_t> b);
@@ -34,14 +35,14 @@ maximum value of the type of the output type.\n
 This routine is only applicable when both the input and output  types are
 integers.
 
-tparam T The input if of type T
+tparam T The input if of type T (T is not deducible)
 tparam U The output is of type U
 
 param z Value to scale and limit
 
 
 *******************************************************************************/
-template <class T, class U>
+template <class T, class U, class  = typename  std::enable_if<std::numeric_limits<T>::is_integer,void>::type>
 T limitScale(U z, unsigned shift)
 {
 	// Verify that we are working with complex of integers value.
@@ -61,6 +62,53 @@ T limitScale(U z, unsigned shift)
 
 }
 
+
+
+/***************************************************************************//**
+Apply the specified right shift to the complex z then limit the real part and
+imaginary part of z to the maximum value of the type of the output complex.\n
+The enable_if restrict this function to be instantiated only when U and T are not
+integer (ie they are complex)
+
+This routine is only applicable when both the input and output complex types are
+integers.
+
+tparam T The input complex is of type complex<T> (T is not deducible)
+tparam U The output complex is of type complex<U>
+
+param z Value to scale and limit
+
+
+*******************************************************************************/
+template <class T, class U, class =  typename std::enable_if<!std::numeric_limits<T>::is_integer, void>::type>
+T limitScale(U z, unsigned shift)
+{
+	// Verify that we are working with complex of integers value.
+	static_assert(std::numeric_limits<typename U::value_type>::is_integer && std::numeric_limits<typename T::value_type>::is_integer, "");
+	// Verify that U has at least as many bits than T
+	static_assert(std::numeric_limits<typename U::value_type>::digits >= std::numeric_limits<typename T::value_type>::digits, "");
+	U a, b;
+
+	a = z.real() >> shift;
+	// Limit the real part
+	if (a > std::numeric_limits<typename T::value_type> :: max() )
+		a = std::numeric_limits<typename T::value_type> :: max();
+	else if (a < std::numeric_limits<typename T::value_type>::lowest())
+		a = std::numeric_limits<typename T::value_type> :: lowest();
+
+	b = z.imag() >> shift;
+	// Limit the imaginary part
+	if (b > std::numeric_limits<typename T::value_type> :: max() )
+		b = std::numeric_limits<typename T::value_type> :: max();
+	else if (b < std::numeric_limits<typename T::value_type>::lowest())
+		b = std::numeric_limits<typename T::value_type> :: lowest();
+
+	return T(a,b);
+
+}
+
+
+#if 0
 /***************************************************************************//**
 Apply the specified right shift to the complex z then limit the real part and
 imaginary part of z to the maximum value of the type of the output complex.\n
@@ -68,7 +116,7 @@ imaginary part of z to the maximum value of the type of the output complex.\n
 This routine is only applicable when both the input and output complex types are
 integers.
 
-tparam T The input complex is of type complex<T>
+tparam T The input complex is of type complex<T> (T is not deducible)
 tparam U The output complex is of type complex<U>
 
 param z Value to scale and limit
@@ -102,7 +150,7 @@ std::complex<T> limitScale(std::complex<U> z, unsigned shift)
 
 }
 
-
+#endif
 
 /**
  * Multiply 2 complex numbers of int16_t then scale the output according to the
